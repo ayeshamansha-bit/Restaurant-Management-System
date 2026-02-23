eec9e08f80cc580e41c3466d5eb9f83d2347d061
ayeshamansha-bit <ayeshamansha64@gmail.com>
2026-02-23 05:03:53 +0500

    Add files via upload

--- restaurant.cpp
+++ restaurant.cpp
@@ -2,17 +2,19 @@
 #include <fstream>
 #include <iomanip>
 using namespace std;
+
 const int MAX = 50;
+
 struct Food
 {
     int id;
     string name;
     string category;
     float price;
-};
+}F;
 
 Food menu[MAX];
-int totalItems = 0;
+int totalItems = 0,count=0;
 
 // Function Prototypes
 void loadFile();
@@ -39,7 +41,7 @@ void loadFile()
     {
         totalItems++;
     }
-
+    
     file.close();
 }
 
@@ -106,18 +108,12 @@ void defaultMenu()
 // Display Menu
 void showMenu()
 {
-    cout << "\n================ FOOD MENU ================\n";
-    cout << left << setw(5) << "ID"
-         << setw(15) << "Name"
-         << setw(15) << "Category"
-         << setw(10) << "Price" << endl;
-
+    cout << "\n\t\t\t\t================== FOOD MENU ==================\n";
+    cout << "\t\t\t\t  "<<left<<setw(5)<<"ID"<<setw(15)<<"Name"<<setw(15)<<"Catagory"<<setw(10)<<"Price"<<endl;
+        
     for (int i = 0; i < totalItems; i++)
     {
-        cout << left << setw(5) << menu[i].id
-             << setw(15) << menu[i].name
-             << setw(15) << menu[i].category
-             << setw(10) << menu[i].price << endl;
+        cout <<"\t\t\t\t  "<<left<<setw(5)<< menu[i].id<<setw(15)<<menu[i].name<<setw(15)<<menu[i].category<<setw(10)<<menu[i].price<<endl;
     }
 }
 
@@ -126,29 +122,29 @@ void addItem()
 {
     Food *ptr = &menu[totalItems];
 
-    cout << "Enter ID: ";
+    cout << "\t\t\t\t\t     Enter ID: ";
     cin >> ptr->id;
 
-    cout << "Enter Name: ";
+    cout << "\t\t\t\t\t     Enter Name: ";
     cin >> ptr->name;
 
-    cout << "Enter Category: ";
+    cout << "\t\t\t\t\t     Enter Category: ";
     cin >> ptr->category;
 
-    cout << "Enter Price: ";
+    cout << "\t\t\t\t\t     Enter Price: ";
     cin >> ptr->price;
 
     totalItems++;
     saveFile();
 
-    cout << "Item Added Successfully!\n";
+    cout << "\t\t\t\t\t     Item Added Successfully!\n";
 }
 
 // Delete Item
 void deleteItem()
 {
     int id;
-    cout << "Enter ID to Delete: ";
+    cout << "\t\t\t\t\t     Enter ID to Delete: ";
     cin >> id;
 
     for(int i=0;i<totalItems;i++)
@@ -160,34 +156,34 @@ void deleteItem()
 
             totalItems--;
             saveFile();
-            cout << "Item Deleted!\n";
+            cout << "\t\t\t\t\t     Item Deleted!\n";
             return;
         }
     }
 
-    cout << "Item Not Found!\n";
+    cout << "\t\t\t\t\t     Item Not Found!\n";
 }
 
 // Update Price
 void updatePrice()
 {
     int id;
-    cout << "Enter ID to Update: ";
+    cout << "\t\t\t\t\t     Enter ID to Update: ";
     cin >> id;
 
     for(int i=0;i<totalItems;i++)
     {
         if(menu[i].id==id)
         {
-            cout << "Enter New Price: ";
+            cout << "\t\t\t\t\t     Enter New Price: ";
             cin >> menu[i].price;
             saveFile();
-            cout << "Price Updated!\n";
+            cout << "\t\t\t\t\t     Price Updated!\n";
             return;
         }
     }
 
-    cout << "Item Not Found!\n";
+    cout <<"\t\t\t\t\t     Item Not Found!\n";
 }
 
 // Admin Panel
@@ -197,13 +193,13 @@ void adminPanel()
 
     do
     {
-        cout << "\n----- ADMIN PANEL -----\n";
-        cout << "1. View Menu\n";
-        cout << "2. Add Item\n";
-        cout << "3. Delete Item\n";
-        cout << "4. Update Price\n";
-        cout << "5. Exit\n";
-        cout << "Choice: ";
+        cout << "\n\t\t\t\t================== Admin Panel==================\n";
+        cout << "\t\t\t\t\t  1. View Menu\n";
+        cout << "\t\t\t\t\t  2. Add Item\n";
+        cout << "\t\t\t\t\t  3. Delete Item\n";
+        cout << "\t\t\t\t\t  4. Update Price\n";
+        cout << "\t\t\t\t\t  5. Exit\n";
+        cout << "\t\t\t\t\t    Choice: ";
         cin >> choice;
 
         switch(choice)
@@ -212,8 +208,8 @@ void adminPanel()
             case 2: addItem(); break;
             case 3: deleteItem(); break;
             case 4: updatePrice(); break;
-            case 5: cout<<"Exiting Admin Panel\n"; break;
-            default: cout<<"Invalid Choice\n";
+            case 5: cout<<"\t\t\t\t\t     Exiting Admin Panel\n"; break;
+            default:cout<<"\t\t\t\t\t     Invalid Choice\n";
         }
 
     }while(choice!=5);
@@ -224,87 +220,107 @@ void adminLogin()
 {
     string user, pass;
 
-    cout << "Enter Username: ";
+    cout << "\t\t\t\t\t     Enter Username: ";
     cin >> user;
 
-    cout << "Enter Password: ";
+    cout << "\t\t\t\t\t     Enter Password: ";
     cin >> pass;
 
     if(user=="admin" && pass=="1234")
         adminPanel();
     else
-        cout << "Invalid Login!\n";
+        cout << "\t\t\t\t\t     Invalid Login!\n";
 }
 
 // Customer Panel
 void customerPanel()
 {
-    string name;
-    cout << "Enter Customer Name: ";
+    int count=0,CartId[count],CartQty[count];  
+    string name,CartName[count];
+    float CartPrice[count];
+    cout << "\t\t\t\t\t     Enter Customer Name: ";
     cin >> name;
 
-    int id, quantity;
+    int id, quantity,choice;
     char more;
     float bill=0;
 
     do
     {
-        showMenu();
-
-        cout << "Enter Item ID: ";
-        cin >> id;
-
-        cout << "Enter Quantity: ";
-        cin >> quantity;
+        cout<<"\n\t\t\t\t\t What you want?"<<endl;
+        cout<<"\t\t\t\t\t  1: View menu."<<endl;
+        cout<<"\t\t\t\t\t  2: Order Food."<<endl;
+        cout<<"\t\t\t\t\t  3: Generate Bill."<<endl;
+        cout<<"\t\t\t\t\t  4: Exit."<<endl;
+        cout<<"\t\t\t\t\t     Enter your choice: ";
+        cin >>choice;
+        switch(choice){
+        	case 1:
+        		showMenu();   break;
+        	case 2:
+			   do{
+			    cout<<"\t\t\t\t\t     Enter food id: ";
+				cin>>id;		
+                cout<<"\t\t\t\t\t     Enter Quantity: ";
+                cin >> quantity;
 
         for(int i=0;i<totalItems;i++)
         {
             if(menu[i].id==id)
             {
-                bill += menu[i].price * quantity;
-                cout<<"Added to Cart!\n";
+            //Storing order in cart
+                
+				 bill += menu[i].price * quantity;
+                cout<<"\t\t\t\t\t     Added to Cart!\n";
             }
         }
-
-        cout << "Order More? (y/n): ";
-        cin >> more;
-
+         cout<< "\t\t\t\t\t     Order More? (y/n): ";
+         cin >> more;
     }while(more=='y'||more=='Y');
-
-    cout << "\n======= BILL =======\n";
-    cout << "Customer: " << name << endl;
-    cout << "Total Bill: " << bill << endl;
-    cout << "Thank You!\n";
+    break;
+    case 3:
+       cout << "\n\t\t\t\t================== BILL ==================\n";
+       cout << "\t\t\t\t\t     Customer: " << name << endl;
+       cout << "\t\t\t\t\t     Total Bill: " << bill << endl;
+       cout << "\t\t\t\t\t     Thank You!\n";
+       break;
+    case 4:
+    	cout<<"\t\t\t\t\t     Thankyou for coming!"<<endl;
+    	break;
+    default:
+    	cout<<"\t\t\t\t\t     Invalid input."<<endl;
+} }while (choice!=4);
 }
-
 // Main Function
-int main()
-{
+ int main(){
     loadFile();
+
     if(totalItems==0)
         defaultMenu();
+
     int choice;
+
     do
     {
-    cout << "\n                 WELCOME                       \n";
-    cout << "\n===============================================" << endl;
-    cout << "         RESTAURANT MANAGEMENT SYSTEM          " << endl;
-    cout << "===============================================" << endl;
-        cout << " 1. Admin Login\n";
-        cout << " 2. Customer Login\n";
-        cout << " 3. Exit\n";
-        cout << " Enter Choice: ";
+    cout << "\n\t\t\t\t                 WELCOME                       \n";
+    cout << "\n\t\t\t\t===============================================" << endl;
+    cout << "\t\t\t\t         RESTAURANT MANAGEMENT SYSTEM          " << endl;
+    cout << "\t\t\t\t===============================================" << endl;
+        cout << "\t\t\t\t\t  1. Admin Login\n";
+        cout << "\t\t\t\t\t  2. Customer Login\n";
+        cout << "\t\t\t\t\t  3. Exit\n";
+        cout << "\t\t\t\t\t     Enter Choice: ";
         cin >> choice;
+
         switch(choice)
         {
             case 1: adminLogin(); break;
             case 2: customerPanel(); break;
-            case 3: cout<<"Good Bye!\n"; break;
-            default: cout<<"Invalid Choice!\n";
+            case 3: cout<<"\t\t\t\t\t    Good Bye!\n"; break;
+            default:cout<<"\t\t\t\t\t    Invalid Choice!\n";
         }
 
     }while(choice!=3);
 
     return 0;
 }
-
